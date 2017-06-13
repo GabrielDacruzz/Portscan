23533b4bd210937af13cdb51251acff6d4c0bfb0
GabrielDacruzz <gabrieldacruzcruzdagabriel@gmail.com>
2017-06-13 09:40:08 -0300

    Update Portscan.py

--- Portscan.py
+++ Portscan.py
@@ -1,2 +1,8 @@
-# Portscan
-este script foi feito com o intuido de averiguar portas abertas
+import socket
+ip = raw_input('digite o ip que desejado: ')
+port = input('digite a porta desejada: ')
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+if sock.connect_ex((ip,port)):
+    print "porta",port,"esta aberta"
+else:
+    print "porta",port,"esta fechada"
